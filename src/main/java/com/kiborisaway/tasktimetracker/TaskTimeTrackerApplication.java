0b7e9b8809f22f6e47980e59f801ad7a7bdba526
saway <sawa.y918@gmail.com>
2026-04-17 15:09:47 +0900

    initial commit

---
+++ src/main/java/com/kiborisaway/tasktimetracker/TaskTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.kiborisaway.tasktimetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTimeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiborisaway/tasktimetracker/TaskTimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiborisaway.tasktimetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
