49b142a59d26350dbf72738f8256d3841be8c64b
saway <sawa.y918@gmail.com>
2026-04-23 11:14:58 +0900

    feat: Projectオブジェクトにバリデーションを実装し、バリデーションの動作確認テストも実装

--- src/main/java/com/kiborisaway/tasktimetracker/data/Project.java
+++ src/main/java/com/kiborisaway/tasktimetracker/data/Project.java
@@ -1,5 +1,10 @@
 package com.kiborisaway.tasktimetracker.data;
 
+import com.kiborisaway.tasktimetracker.validation.CreateGroup;
+import com.kiborisaway.tasktimetracker.validation.UpdateGroup;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,8 +18,14 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Project {
 
+  @NotNull(groups = UpdateGroup.class)
   private Integer id;
+
+  @NotBlank(groups = {CreateGroup.class, UpdateGroup.class})
+  @Size(max = 20, groups = {CreateGroup.class, UpdateGroup.class})
   private String title;
+
+  @Size(max = 200, groups = {CreateGroup.class, UpdateGroup.class})
   private String description;
 
 }
