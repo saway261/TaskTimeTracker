161403edbf27c8c8c27da5527971adfb4cba3c8c
saway <sawa.y918@gmail.com>
2026-07-23 22:30:10 +0900

    feat: TaskGroupオブジェクトにアクセサなどを追加　また、isFinishedフラグを追加

--- src/main/java/com/kiborisaway/tasktimetracker/data/TaskGroup.java
+++ src/main/java/com/kiborisaway/tasktimetracker/data/TaskGroup.java
@@ -1,11 +1,22 @@
 package com.kiborisaway.tasktimetracker.data;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskGroup {
 
   private Integer id;
   private Integer projectId;
   private String title;
   private String description;
-  private Integer displayOrder;
+  private Boolean isFinished;
 
 }
